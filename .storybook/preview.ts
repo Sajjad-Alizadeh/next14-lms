68a5f4a8e7899319126213db60e0f5d79cdab28b
Sajjad Alizadeh <sajjadalizzh@gmail.com>
2024-05-15 18:27:52 +0330

    Add some notes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
 import type { Preview } from "@storybook/react";
 
+// Apply tailwind css to story book
 import '../src/app/globals.css';
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
