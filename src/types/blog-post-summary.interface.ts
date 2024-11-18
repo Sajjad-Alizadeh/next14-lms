d14898d6fce5fbc9719c3f72f817523078488ff0
Sajjad Alizadeh <sajjadalizzh@gmail.com>
2024-11-18 16:34:17 +0330

    Impl blog section

---
+++ src/types/blog-post-summary.interface.ts
@@ -0,0 +1,13 @@
+export interface BlogPostSummary {
+  title: string,
+  slug: string,
+  postDate: string,
+  thumbnailUrl: string,
+  studyTime: number,
+  isNew: boolean,
+  numberOfViews: number,
+  author: string,
+  numberOfComments: number,
+  summary: string,
+  category: string
+}
