28bc98138aeba6e03ded4e9da43d94515c1a35dd
Sajjad Alizadeh <sajjadalizzh@gmail.com>
2024-11-24 13:08:58 +0330

    Impl rating component

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import {CourseDetails} from "@/types/course-details.interface";
 import {API_URL} from "@/configs/global";
+import {Rating} from "@/app/_components/rating";
 
 export async function generateStaticParams() {
   const slugs = await fetch(`${API_URL}/courses/slugs`).then(res => res.json());
@@ -16,10 +17,26 @@ async function getCourseDetails(slug: string): Promise<CourseDetails> {
 
 export default async function CourseDetailsPage({params}: { params: { slug: string } }) {
   const {slug} = params;
-  const courseData = await getCourseDetails(slug)
+  const course = await getCourseDetails(slug)
   return (
-    <div className="text-5xl flex justify-center items-center w-full">
-      <h1>{courseData.title}</h1>
+    <div className="h-96 container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
+      <div
+        className="bg-primary pointer-events-none absolute left-1/2 aspect-square w-1/2 -translate-x-1/2 -top-96 rounded-full opacity-10 blur-3xl"></div>
+      <div className="col-span-10 xl:col-span-7">
+        <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
+          {course.title}
+        </h1>
+        <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
+          {course.subTitle}
+        </h2>
+
+        <div className=" mt-5">Video Player Component</div>
+      </div>
+      <div className="col-span-10 xl:col-span-3">
+        <Rating rate={3}/>
+      </div>
+      <div className="col-span-10 xl:col-span-6 bg-accent"></div>
+      <div className="col-span-10 xl:col-span-4 bg-warning"></div>
     </div>
   )
 }
