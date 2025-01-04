65f3f91036c33c2cbfefd55920a88683b0ed8708
Sajjad Alizadeh <sajjadalizzh@gmail.com>
2025-01-04 17:21:58 +0330

    Complete verification form

--- src/app/(auth)/verify/_components/verification-form.tsx
+++ src/app/(auth)/verify/_components/verification-form.tsx
@@ -4,27 +4,76 @@ import AuthCode from "@/app/_components/auth-code/auth-code";
 import { AuthCodeRef } from "@/app/_components/auth-code/auth-code.types";
 import { Button } from "@/app/_components/button/button";
 import Link from "next/link";
-import { useRef } from "react";
+import {useRef, useState} from "react";
+import {Timer} from "@/app/_components/timer/timer";
+import {TimerRef} from "@/app/_components/timer/timer.types";
+import {useForm} from "react-hook-form";
+import {VerifyUserModel} from "@/app/(auth)/verify/_types/verify-user.type";
+import {useNotificationStore} from "@/stores/notification.store";
+import {useSearchParams} from "next/navigation";
+import {useSendAuthCode} from "@/app/(auth)/verify/_api/send-auth-code";
+
+const getTwoMinutesFromNow = () => {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 5);
+  return time;
+};
 
 const VerificationForm = () => {
+  const [showResendCode, setShowResendCode] = useState<boolean>(false);
   const authCodeRef = useRef<AuthCodeRef>(null);
+  const timerRef = useRef<TimerRef>(null);
+
+  const {handleSubmit, setValue, register, formState: {isValid}} = useForm<VerifyUserModel>();
+
+  const showNotification = useNotificationStore(state => state.showNotification);
+
+  const params = useSearchParams();
+  const username = params.get('mobile')!;
+
+  const sendAuthCode = useSendAuthCode({
+    onSuccess: () => {
+      showNotification({
+        type: 'info',
+        message: 'کد تایید به شماره شما ارسال شد'
+      })
+    }
+  })
+
+  const onSubmit = (data: VerifyUserModel) => {
+    data.username = username;
+    console.log(data);
+  }
+
+  register('code', {
+    validate: (value: string) => (value ?? "").length === 5
+  })
+
+  const resendAuthCode = () => {
+    timerRef.current?.restart(getTwoMinutesFromNow());
+    setShowResendCode(false);
+    sendAuthCode.submit(username);
+    authCodeRef.current?.clear();
+  }
+
   return (
     <>
       <h5 className="text-2xl">کد تایید</h5>
       <p className="mt-2">
         دنیای شگفت انگیز برنامه نویسی در انتظار شماست!
       </p>
-      <form className="flex flex-col gap-6 mt-10 flex-1">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6 mt-10 flex-1">
         <AuthCode
           className="mt-10"
           ref={authCodeRef}
           onChange={(value) => {
-            console.log(value);
+            setValue('code', value, {shouldValidate: true})
           }}
         />
-        Timer
-        <Button isLink={true} onClick={authCodeRef.current?.clear}>ارسال مجدد کد تایید</Button>
-        <Button type="submit" variant="primary">
+        <Timer ref={timerRef} className="my-8" size="small" onExpire={()=> {setShowResendCode(true)}} expiryTimestamp={getTwoMinutesFromNow()} showDays={false} showHours={false}/>
+
+        <Button isLink={true} isDisabled={!showResendCode} onClick={resendAuthCode}>ارسال مجدد کد تایید</Button>
+        <Button type="submit" variant="primary" isDisabled={!isValid}>
           تایید و ادامه
         </Button>
         <div className="flex items-start gap-1 justify-center mt-auto">
