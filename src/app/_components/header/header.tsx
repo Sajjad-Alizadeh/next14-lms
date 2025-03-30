2b55041b4dbb97fa33154ee4835ce0d1e5da2522
Sajjad Alizadeh <sajjadalizzh@gmail.com>
2025-03-30 15:27:05 +0330

    Add route handler for next auth

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import { Navbar } from "./navbar";
+// import {auth} from "../../../../auth";
+import HeaderUserSection from "@/app/_components/header/header-user-section";
 
-export const Header: React.FC = () => {
+export const Header: React.FC = async () => {
+  // const test = await auth() ==> if we use this line, all page converted to the dynamic rendering. because with this line
+  // we have access to the cookies, make it dynamic rendering and this component used in main layout and convert all page
+  // to the dynamic rendering
+
   return (
     <header className="border-b dark:border-base-content dark:border-opacity-5">
       <div className="container flex items-center justify-between">
@@ -12,7 +18,9 @@ export const Header: React.FC = () => {
           height={36}
         />
         <Navbar />
-        <span className="mr-auto">User Authentication</span>
+        <span className="mr-auto">
+          <HeaderUserSection/>
+        </span>
       </div>
     </header>
   );
