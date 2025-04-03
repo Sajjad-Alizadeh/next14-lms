fcbdc5934e2963266614eb92e6808cc5949a8095
Sajjad Alizadeh <sajjadalizzh@gmail.com>
2025-04-03 12:24:42 +0330

    Prevent from access the auth pages when user logged in

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,31 @@
 import {NextAuthConfig} from "next-auth";
 
 export const authConfig = {
-  pages : {
+  pages: {
     signIn: '/signin',
   },
   callbacks: {
+    async authorized({auth, request}) {
+      const {nextUrl} = request
 
+      const isAuthenticated = !!auth?.user
+
+      const authRoutes = ['/signin', '/verify']
+      const protectedPaths = ['student'] // Note: don't use '/' before path names
+      const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+      const firstPath = nextUrl.pathname.split('/')[1]
+      const isProtectedRoute = protectedPaths.includes(firstPath)
+
+      if (isAuthenticated && isAuthRoute) {
+        return Response.redirect(new URL('/student/courses', nextUrl))
+      }
+
+      if (!isAuthenticated && isProtectedRoute) {
+        return Response.redirect(new URL('/signin', nextUrl))
+      }
+
+      return true
+    }
   },
   providers: []
 } satisfies NextAuthConfig
--- src/app/(accounts)/student/layout.tsx
+++ src/app/(accounts)/student/layout.tsx
@@ -1,18 +1,8 @@
-import {auth} from "../../../../auth";
-import {redirect} from "next/navigation";
-
 export default async function StudentLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await auth()
-
-  if (!session || !session.user) {
-    redirect("/signin");
-  }
-
-
   return (
     <>
       <aside className="bg-gray-300 w-80 flex justify-center items-center self-stretch text-3xl">
